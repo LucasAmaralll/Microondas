4f87d886497a9da7ac7c7a508f3825fd666ea55a
Lucas do Amaral Faustino <lukinhafaustino2001@gmail.com>
2024-07-08 23:26:43 -0300

    second commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,8 @@
+// Adicione estes componentes ao Form1.Designer.cs
+private System.Windows.Forms.TextBox txtNomePrograma;
+private System.Windows.Forms.TextBox txtAlimento;
+private System.Windows.Forms.TextBox txtTempoPrograma;
+private System.Windows.Forms.TextBox txtPotenciaPrograma;
+private System.Windows.Forms.TextBox txtCaractere;
+private System.Windows.Forms.TextBox txtInstrucoes;
+private System.Windows.Forms.Button btnAdicionarPrograma;
